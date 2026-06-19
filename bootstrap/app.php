42a6da062085475c7aca38d71800d14516ae2660
Sauki Annaim <saukiannaim046@gmail.com>
2026-06-19 12:24:48 +0700

    Fix Laravel runtime storage on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,13 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__));
+
+if (getenv('VERCEL') || getenv('VIEW_COMPILED_PATH')) {
+    $app->useStoragePath('/tmp/laravel-storage');
+}
+
+return $app
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
